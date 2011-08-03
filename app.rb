5d370ae9754116d8c00265146fd700be748a2a63
Ryan Rauh <rauh.ryan@gmail.com>
2011-08-03 13:45:06 -0500

    added an all builds option

--- app.rb
+++ app.rb
@@ -5,44 +5,72 @@ require 'net/http'
 require 'crack'
 require 'nokogiri'
 
-DOMAIN = "teamcity.codebetter.com"
-#DOMAIN = "http://teamcity:8082"
+#DOMAIN = "teamcity.codebetter.com"
+DOMAIN = "teamcity"
+PORT = 8082
 URL = "#{DOMAIN}/guestAuth/app/rest/"
 
 get '/projects' do 
-    content_type :json
+	content_type :json
 
-    http = Net::HTTP.new(DOMAIN)
-    resp, data = http.get("/guestAuth/app/rest/projects", {'Accept', "application/json"})
-    data
+	http = Net::HTTP.new(DOMAIN,PORT)
+	resp, data = http.get("/guestAuth/app/rest/projects", {'Accept', "application/json"})
+	data
 end
 
 get "/projects/:id" do |id|
-    content_type :json
+	content_type :json
 
-    http = Net::HTTP.new(DOMAIN)
-    resp, data = http.get("/guestAuth/app/rest/projects/id:#{id}/buildTypes", {'Accept', "application/json"})
-    
-    buildTypes = Crack::JSON.parse(data)
+	http = Net::HTTP.new(DOMAIN,PORT)
+	resp, data = http.get("/guestAuth/app/rest/projects/id:#{id}/buildTypes", {'Accept', "application/json"})
 
-    buildTypes["buildType"].each do |build|
-        
-      response, json = http.get("/guestAuth/app/rest/builds/?locator=buildType:#{build["id"]},count:5", {'Accept', "application/json"})
-    
-      build["history"] = Crack::JSON.parse(json)["build"]
-    end
+	buildTypes = Crack::JSON.parse(data)
 
-    buildTypes.to_json
+	buildTypes["buildType"].each do |build|
+
+		response, json = http.get("/guestAuth/app/rest/builds/?locator=buildType:#{build["id"]},count:5", {'Accept', "application/json"})
+
+		build["history"] = Crack::JSON.parse(json)["build"]
+	end
+
+	buildTypes.to_json
+end
+
+get '/builds/all' do 
+	content_type :json
+
+	http = Net::HTTP.new(DOMAIN, PORT)
+	resp, data = http.get("/guestAuth/app/rest/projects", {'Accept', "application/json"})
+
+	projects = Crack::JSON.parse(data);
+
+	projects["project"].each do |project|
+
+		project["builds"] = []
+		resp, data = http.get("/guestAuth/app/rest/projects/id:#{project["id"]}/buildTypes", {'Accept', "application/json"})
+
+		buildTypes = Crack::JSON.parse(data)
+
+		buildTypes["buildType"].each do |build|
+
+			response, json = http.get("/guestAuth/app/rest/builds/?locator=buildType:#{build["id"]},count:5", {'Accept', "application/json"})
+
+			build["history"] = Crack::JSON.parse(json)["build"]
+			project["builds"] << build
+		end
+	end
+
+	projects.to_json
 end
 
 
 get '/' do 
-    erb :index
+	erb :index
 end
 
 def api(route, &block)
-    http = Net::HTTP.new(DOMAIN)
-    resp, data = http.get(route, {'Accept', "application/json"})
-    block.call data
+	http = Net::HTTP.new(DOMAIN)
+	resp, data = http.get(route, {'Accept', "application/json"})
+	block.call data
 end
 
